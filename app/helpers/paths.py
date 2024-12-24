1323d63dc4092370d0347bb63ef15133c2937c6b
Henrik Karlsson <sven.henrik.karlsson@gmail.com>
2024-12-24 19:50:17 +0100

    feat: Create necessary directories for audio and memory storage

--- app/helpers/paths.py
+++ app/helpers/paths.py
@@ -16,3 +16,13 @@ GENERATED_AUDIO_DIR = AUDIO_DIR / "generated"
 ASSETS_DIR = APP_DIR / "assets"
 CHIME_PATH = ASSETS_DIR / "chime.mp3"
 TIMER_ALARM_PATH = ASSETS_DIR / "timer-alarm.mp3"
+
+# Creating all the necessary directories
+for dir_ in [
+    SHORT_TERM_MEMORY_DIR,
+    AUDIO_DIR,
+    MESSAGES_DIR,
+    RECORDED_AUDIO_DIR,
+    GENERATED_AUDIO_DIR,
+]:
+    dir_.mkdir(exist_ok=True, parents=True)
